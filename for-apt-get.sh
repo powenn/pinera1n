59278c5806e48b8cdebc09ed54045b92081b478d
powen <powen0966091716@gmail.com>
2021-11-06 23:09:32 +0800

    Update for-apt-get.sh

--- for-apt-get.sh
+++ for-apt-get.sh
@@ -6,7 +6,6 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install xterm usbmuxd curl libusbmuxd-tools
 wget https://assets.checkra.in/downloads/linux/cli/arm64/43019a573ab1c866fe88edb1f2dd5bb38b0caf135533ee0d6e3ed720256b89d0/checkra1n
-cp -R /Downloads/checkra1n /Home
 chmod +x checkra1n
 echo ""
 echo "⣿⣿⣿⠟⠛⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢋⣩⣉⢻"
