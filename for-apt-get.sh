dc7a151de82aed8c53d933729e3137aebad3c334
powen <powen0966091716@gmail.com>
2021-11-06 23:12:13 +0800

    Update for-apt-get.sh

--- for-apt-get.sh
+++ for-apt-get.sh
@@ -4,7 +4,7 @@ echo "now installing dependencies"
 
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get install xterm usbmuxd curl libusbmuxd-tools
+sudo apt-get install xterm usbmuxd curl libusbmuxd-tools wget 
 wget https://assets.checkra.in/downloads/linux/cli/arm64/43019a573ab1c866fe88edb1f2dd5bb38b0caf135533ee0d6e3ed720256b89d0/checkra1n
 chmod +x checkra1n
 echo ""
